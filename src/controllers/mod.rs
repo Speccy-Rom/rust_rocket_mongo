574acd64a7b6ca93e096946fb6a9a2d944fdc428
speccy-rom <speccy.rom@gmail.com>
2022-04-12 22:14:22 +0300

    create: controller mod generic_response

---
+++ src/controllers/mod.rs
@@ -0,0 +1,14 @@
+pub mod index;
+pub mod users;
+
+use rocket::{http::Status, serde::json::Value};
+use serde::Serialize;
+
+use crate::error::Error;
+
+fn generic_response<T: Serialize>(result: Result<T, Error>) -> (Status, Value) {
+    match result {
+        Ok(data) => json_response!(data),
+        Err(e) => json_response!(e.to_status().code, e.to_string()),
+    }
+}
